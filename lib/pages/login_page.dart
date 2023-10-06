80e4cf7db58c40bbb6c339927c3aa0f96faf7bbd
FarisET <faris.ejaz16@gmail.com>
2023-10-06 09:46:59 +0500

    FIRST COMMIT

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,79 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _isObscure = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SHRINE'),
+        leading: IconButton(
+            icon: const Icon(Icons.menu, semanticLabel: 'menu'),
+            onPressed: () {
+              print('Menu button');
+            }),
+      ),
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: ListView(children: [
+          SizedBox(
+            height: 80,
+          ),
+          Column(
+            children: [
+              Image.asset('assets/diamond.png'),
+              SizedBox(
+                height: 80,
+              ),
+              Text('Shrine'),
+              SizedBox(
+                height: 80,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  labelText: 'Username',
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              TextField(
+                  obscureText: _isObscure,
+                  decoration: InputDecoration(
+                    filled: true,
+                    labelText: 'Password',
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                          _isObscure ? Icons.visibility : Icons.visibility_off),
+                      onPressed: () {
+                        setState(() {
+                          _isObscure = !_isObscure;
+                        });
+                      },
+                    ),
+                  )),
+              OverflowBar(
+                alignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(onPressed: () {}, child: Text('CANCEL')),
+                  ElevatedButton(onPressed: () {}, child: Text('NEXT'))
+                ],
+              )
+            ],
+          )
+        ]),
+      )),
+    );
+  }
+}
